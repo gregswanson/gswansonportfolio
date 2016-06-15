46acdfde0b2ba1c9081d9254ae2044973531150a
Gregory Swanson <gswanson@gregorys-mbp.fios-router.home>
2016-06-14 21:57:23 -0400

    preloader

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,3 +1,18 @@
+
+///Preloader///////
+$(window).load(function() { // makes sure the whole site is loaded
+     var preloader=$('#mask div');
+     preloader.fadeOut(); // will first fade out the loading animation
+     $('#mask').delay(350).fadeOut('slow'); 
+    // will fade out the white DIV that covers the website.
+     $('body').delay(350).css({'overflow':'visible'});
+  }) 
+
+
+
+
+
+
 //email
 
 
