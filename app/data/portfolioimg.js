dbd376abe350114a2bab06f7006c26fd7a933b8d
Gregory Swanson <gswanson@Gregorys-MBP.home>
2016-07-26 17:37:20 -0400

    api

--- app/data/portfolioimg.js
+++ app/data/portfolioimg.js
@@ -102,7 +102,7 @@ var portfolioimg = [
 	noteone: 'Brochure',
 	notetwo: '28 page brochure',
 	popup: 'xxxx',
-	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c6b7e4b08c1a725d4f53/53a9c6d0e4b0ba352f67c940/1403635409434/Xiaflex_2_4_mu.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c6b7e4b08c1a725d4f53/53a9c6d0e4b08c1a725d4f82/1403635409528/Xiaflex_2_3_mu.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c6b7e4b08c1a725d4f53/53a9c6d0e4b08c1a725d4f84/1403635409299/Xiaflex_2_2_mu.jpg'],
+	imgs: ['https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/Xiaflex_2_4_mu.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/Xiaflex_2_2_mu.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/Xiaflex_2_3_mu.png'],
 	desc: '28-page brochure for a pharmaceutical client.',
 	tech: 'Technologies used: Photoshop, Illustrator, InDesign'
 	},
@@ -113,7 +113,7 @@ var portfolioimg = [
 	noteone: 'Brochure',
 	notetwo: '8 page brochure',
 	popup: 'xxxx',
-	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c7a3e4b0de487b5a84e3/53a9c878e4b0918746e034a2/1403635875457/SIUH1a_mu2.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c7a3e4b0de487b5a84e3/53a9c878e4b0ac3522f5f797/1403635858602/SIUH2_mu2.jpg'],
+	imgs: ['https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/SIUH1a_mu2.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/SIUH2_mu2.png'],
 	desc: 'Informational booklet describing hospital\'s new emergency and trauma center.',
 	tech: 'Technologies used: Photoshop, Illustrator, InDesign'
 	},
@@ -124,7 +124,7 @@ var portfolioimg = [
 	noteone: 'Record Artwork',
 	notetwo: '7\" record',
 	popup: 'xxxx',
-	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cf7ee4b07db2cfa19bfe/53a9cf9ce4b077fee360a24e/1403637682822/TAJ1.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cf7ee4b07db2cfa19bfe/53a9cf9ce4b077fee360a250/1403637669201/TAJ2.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cf7ee4b07db2cfa19bfe/53a9cf9ce4b07b097d155c87/1403637668865/VelocityAmberCVR.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cf7ee4b07db2cfa19bfe/53a9cf9ce4b07b097d155c89/1403637681638/Label_Art.jpg'],
+	imgs: ['https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/TAJ1.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/TAJ2.png'],
 	desc: 'Split 7\" vinyl record for Velocity and The Amber Jets released in US and Japan',
 	tech: 'Technologies used: Photoshop, Illustrator, InDesign'
 	},
@@ -135,7 +135,7 @@ var portfolioimg = [
 	noteone: 'Music Posters',
 	notetwo: 'various',
 	popup: 'xxxx',
-	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c8f0e4b08c1a725d5427/53a9c916e4b03de9feee1fad/1403635991476/Japanposter2C_mu.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c8f0e4b08c1a725d5427/53a9c917e4b058df6ed1f682/1403635991356/120410_Walter_mu.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c8f0e4b08c1a725d5427/53a9c961e4b0f2b2922ba1d2/1403636070750/TAJ_DSOD_052209_mu.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c8f0e4b08c1a725d5427/53a9c918e4b03de9feee1fb5/1403635992764/Sweeps_2_mu.jpg'],
+	imgs: ['https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/Japanposter2C_mu.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/120410_Walter_mu.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/TAJ_DSOD_052209_mu.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/Sweeps_2_mu.png'],
 	desc: 'Collection of posters promoting various live concert events.',
 	tech: 'Technologies used: Photoshop, Illustrator, InDesign'
 	},
@@ -166,7 +166,7 @@ var portfolioimg = [
 	noteone: 'Landing Pages',
 	notetwo: 'virtual conference',
 	popup: 'xxxx',
-	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cab6e4b09b44e37a526e/53a9cae2e4b05b89323292a7/1403636461054/vcopious_Screens.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cab6e4b09b44e37a526e/53a9cae3e4b05b89323292a9/1403636452659/vcopious_ECFD.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cab6e4b09b44e37a526e/53a9cae2e4b051ebda169044/1403636479442/vcopious_Virtual_Map.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cab6e4b09b44e37a526e/53a9cb55e4b097b2557bac42/1403636609769/vcopious_Education.jpg'],
+	imgs: ['https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/vcopious_Screens.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/vcopious_ECFD.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/vcopious_Virtual_Map.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/vcopious_Education.png'],
 	desc: 'Opening screens for virtual conference events.',
 	tech: 'Technologies used: Photoshop, Illustrator'
 	},
@@ -197,7 +197,7 @@ var portfolioimg = [
 	noteone: 'Logos',
 	notetwo: 'various',
 	popup: 'xxxx',
-	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa535ee4b06de1f72320df/1403711561927/student_leadership.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa535ee4b06de1f72320e1/1403712299814/ieee_HKN_Logo.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa5588e4b035c356289e7a/1403671950436/alumni_Reconnect.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa4b38e4b0ffc29c24adc5/1403711717525/the_Heavenly_Tenants.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa5789e4b0cb8f6cc4c6f9/1403711742151/dsod.jpg?format=1500w', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa5789e4b05b8379703aea/1403711829036/taj_logo.jpg'],
+	imgs: ['https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/student_leadership.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/ieee_HKN_Logo.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/alumni_Reconnect.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/the_Heavenly_Tenants.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/dsod.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/taj_logo.png'],
 	desc: 'A collection of various logos.',
 	tech: 'Technologies used: Photoshop, Illustrator'
 	},
@@ -208,7 +208,7 @@ var portfolioimg = [
 	noteone: 'Facebook',
 	notetwo: 'company page',
 	popup: 'xxxx',
-	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cd68e4b05b8932329713/53a9cd81e4b0afe50f81e77b/1403637131644/IEEE_Facebook_1.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cd68e4b05b8932329713/53a9cd81e4b05b8932329755/1403637124587/IEEE_Facebook_4.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cd68e4b05b8932329713/53a9cd82e4b05b8932329757/1403637129386/IEEE_Facebook_3.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cd68e4b05b8932329713/53a9cd81e4b0afe50f81e779/1403637136844/IEEE_Facebook_2.jpg' ],
+	imgs: ['https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/IEEE_Facebook_1.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/IEEE_Facebook_4.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/IEEE_Facebook_3.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/IEEE_Facebook_2.png' ],
 	desc: 'Series of screens to promote company\'s various programs.',
 	tech: 'Technologies used: Photoshop, Illustrator'
 	},
@@ -219,7 +219,7 @@ var portfolioimg = [
 	noteone: 'Various',
 	notetwo: 'various pieces',
 	popup: 'xxxx',
-	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d250e4b0afe50f81f079/53ab264ae4b0b04a4fac5eb1/1403725428438/GIE_Patch_2.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d250e4b0afe50f81f079/53ab2669e4b0b04a4fac5ee3/1403725450618/Sticker.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d250e4b0afe50f81f079/550652a5e4b0c3c9c4ca9da1/1426477734398/OHSP_1a_Keep.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d250e4b0afe50f81f079/550652b1e4b0953c7cbebe5d/1426477749626/OHSP_1b_Keep.jpg'],
+	imgs: ['https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/various.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/Sticker.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/OHSP_1a_Keep.png', 'https://github.com/gregswanson/gswansonportfolio/blob/master/app/public/img/portfolio/OHSP_1b_Keep.png'],
 	desc: 'A collection of various projects.',
 	tech: 'Technologies used: Photoshop, Illustraton, InDesign'
 	}
